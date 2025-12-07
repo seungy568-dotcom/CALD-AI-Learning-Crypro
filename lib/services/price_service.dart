8f30ca4c1761308418ff4a2bdbf2e470a23fde3c
seungy568-dotcom <seungy568@gmail.com>
2025-12-07 09:48:12 +0700

    Create price_service.dart

---
+++ lib/services/price_service.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class PriceService {
+  Future<double?> fetchPrice(String symbol) async {
+    final url = "https://api.binance.com/api/v3/ticker/price?symbol=$symbol";
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return double.tryParse(data["price"]);
+    } else {
+      return null;
+    }
+  }
+}
